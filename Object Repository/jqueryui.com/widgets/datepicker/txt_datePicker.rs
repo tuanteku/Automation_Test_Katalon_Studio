9bdae9e43065f4f1983513a26bf154df970e8645
tuantq <tuantq@DN00036.mshome.net>
2019-03-29 16:34:16 +0700

    AAA

---
+++ Object Repository/jqueryui.com/widgets/datepicker/txt_datePicker.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>textbox field is used input date from calendar popup</description>
+   <name>txt_datePicker</name>
+   <tag></tag>
+   <elementGuidId>e5ff96e1-e57e-4ee1-af76-58f07aa81046</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;datepicker&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
